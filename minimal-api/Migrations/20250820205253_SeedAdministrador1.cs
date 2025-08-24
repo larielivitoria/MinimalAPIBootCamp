d8a987be63769f88ecc8babbf0b7eee2a1d7ed21
larielivitoria <larielivitoria@gmail.com>
2025-08-24 14:17:07 -0300

    Adicionando minimal-api como pasta normal

---
+++ minimal-api/Migrations/20250820205253_SeedAdministrador1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace minimal_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAdministrador1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Administradores",
+                columns: new[] { "Id", "Email", "Senha", "Perfil" },
+                values: new object[] { 1, "administrador@teste.com", "123456", "Adm" });
+            
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Administradores",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
